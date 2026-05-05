54bb5d310c0e0bfb02fd6d6032af9c2adbd559cf
geoKim <kgeo6264@gmail.com>
2026-05-05 22:13:03 +0900

    fe실습 3: CRUD

--- src/main/java/likelion14th/blog/controller/ArticleController.java
+++ src/main/java/likelion14th/blog/controller/ArticleController.java
@@ -2,6 +2,7 @@ package likelion14th.blog.controller;
 
 import likelion14th.blog.domain.Article;
 import likelion14th.blog.dto.request.ArticleRequest;
+import likelion14th.blog.dto.request.UpdateArticleRequest;
 import likelion14th.blog.dto.response.ApiResponse;
 import likelion14th.blog.dto.response.ArticleResponse;
 import likelion14th.blog.service.ArticleService;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +26,33 @@ public class ArticleController {
                 articleService.addArticle(request.getTitle(), request.getContent(), request.getAuthor(), request.getPassword());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(201, "게시글 생성에 성공하였습니다.", articleResponse));
+    }
+
+    @GetMapping()
+    public ResponseEntity<ApiResponse<List<ArticleResponse>>> getArticles() {
+        List<ArticleResponse> articleResponses = articleService.getArticles();
+
+        return ResponseEntity.ok(ApiResponse.success(200, "게시글 전체 조회에 성공하였습니다.", articleResponses));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<ArticleResponse>> getOneArticle(@PathVariable Long id) {
+        ArticleResponse articleResponse = articleService.getOneArticle(id);
+
+        return ResponseEntity.ok(ApiResponse.success(200, "게시글 전체 조회에 성공하였습니다.", articleResponse));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApiResponse<ArticleResponse>> updateArticle(@PathVariable Long id, @RequestBody UpdateArticleRequest request) {
+        ArticleResponse articleResponse = articleService.updateArticle(id, request.getTitle(), request.getContent());
+
+        return ResponseEntity.ok(ApiResponse.success(200, "게시글을 업데이트 하였습니다.", articleResponse));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>>  deleteArticle(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponse.success(204,"게시글을 삭제하는데 성공하였습니다."));
     }
 }
--- src/main/java/likelion14th/blog/service/ArticleService.java
+++ src/main/java/likelion14th/blog/service/ArticleService.java
@@ -1,18 +1,25 @@
 package likelion14th.blog.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import likelion14th.blog.domain.Article;
 import likelion14th.blog.dto.request.ArticleRequest;
 import likelion14th.blog.dto.response.ArticleResponse;
 import likelion14th.blog.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ArticleService {
     // 의존성 주입
     private final ArticleRepository articleRepository;
 
+    @Transactional
     public ArticleResponse addArticle(String title, String content, String author, String password) {
         // Article 객체 생성
         Article article = new Article(title, content, author, password);
@@ -23,6 +30,45 @@ public class ArticleService {
         // Article 객체를 response DTO 생성하여 반환
         // response 클래스의 정작 팩토리 메서드 from()을 통해 API 응답객체 생성
         return ArticleResponse.from(article);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ArticleResponse> getArticles() {
+        // Article 전체 객체 리스트 가져오기
+        List<Article> articles = articleRepository.findAll();
+
+        // ArticleResponse 리스트로 변경
+        List<ArticleResponse> articleResponses = articles.stream()
+                .map(ArticleResponse::from)
+                .toList();
+
+        return articleResponses;
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleResponse getOneArticle(Long id) {
+        // 변수로 받은 id를 가지는 Article 가져오기
+        Article article = articleRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다."));
+
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public ArticleResponse updateArticle(Long id, String title, String content){
+        Article article = articleRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다."));;
+
+        article.setTitle(title);
+        article.setContent(content);
+
+        articleRepository.save(article);
+        return ArticleResponse.from(article);
+    }
+
+    @Transactional
+    public Void deleteArticle(Long id){
+        articleRepository.deleteById(id);
+
+        return null;
     }
 }
 
