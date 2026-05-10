09808c4fe1ed2b7b1def3f72765f374df2ced0df
geoKim <kgeo6264@gmail.com>
2026-05-10 22:15:29 +0900

    ëì댓글 생성하기

---
+++ src/main/java/likelion14th/blog/controller/CommentController.java
@@ -0,0 +1,27 @@
+package likelion14th.blog.controller;
+
+import likelion14th.blog.dto.request.CommentRequest;
+import likelion14th.blog.dto.response.ApiResponse;
+import likelion14th.blog.dto.response.ArticleSummaryResponse;
+import likelion14th.blog.dto.response.CommentResponse;
+import likelion14th.blog.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/articles")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{articleId}/comments")
+    public ResponseEntity<ApiResponse<CommentResponse>> addComment(@RequestBody CommentRequest commentRequest, @PathVariable Long articleId) {
+        CommentResponse commentResponse = commentService.addComment(commentRequest.getContent(), commentRequest.getAuthor(), articleId);
+
+        return ResponseEntity.ok(ApiResponse.success(201, "댓글 생성에 성공하였습니다.", commentResponse));
+    }
+}
