aefda1e3ce34ca1526d6316100815c30af81b769
geooeg <kgeo6264@gmail.com>
2026-07-03 16:24:18 +0900

    refact(api): apapi에 /api 추가

--- src/main/java/likelion14th/blog/controller/CommentController.java
+++ src/main/java/likelion14th/blog/controller/CommentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/articles")
+@RequestMapping("/api/articles")
 public class CommentController {
 
     private final CommentService commentService;
